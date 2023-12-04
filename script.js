ffc376fcfdced3ff5d2244d2cc35c0dc1f615490
Ryan Olson <ryan.olson86@yahoo.com>
2023-12-04 16:56:32 -0600

    added all attack functions

--- script.js
+++ script.js
@@ -7,10 +7,32 @@ let freakyFungusHealth = 100;
 let myAttackPoints = 100;
 
 // ! Events
+// Attack functions
 function arcaneScepter(){
     console.log('In arcaneScepter')
     freakyFungusHealth = freakyFungusHealth - 14;
     myAttackPoints = myAttackPoints - 12;
+    console.log('New AP', myAttackPoints);
+    console.log('New HP', freakyFungusHealth);
+}
+function entangle(){
+    console.log('In entangle')
+    freakyFungusHealth = freakyFungusHealth - 9;
+    myAttackPoints = myAttackPoints - 23;
+    console.log('New AP', myAttackPoints);
+    console.log('New HP', freakyFungusHealth);
+}
+function dragonBlade(){
+    console.log('In dragonBlade')
+    freakyFungusHealth = freakyFungusHealth - 47;
+    myAttackPoints = myAttackPoints - 38;
+    console.log('New AP', myAttackPoints);
+    console.log('New HP', freakyFungusHealth);
+}
+function starFire(){
+    console.log('In starFire')
+    freakyFungusHealth = freakyFungusHealth - 25;
+    myAttackPoints = myAttackPoints - 33;
     console.log('New AP', myAttackPoints);
     console.log('New HP', freakyFungusHealth);
 }
